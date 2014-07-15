84b9a1b31842ceda03083f361fa09e648545d175
Jaydi11a <geraldtrotman@fas.harvard.edu>
2014-07-14 23:48:32 -0400

    added random user package and alias

--- app/config/app.php
+++ app/config/app.php
@@ -189,6 +189,7 @@ return array(
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
 		'lorem-ipsum'	  => 'badcow/lorem-ipsum',
+		'faker'			  => 'fzaninotto/faker',
 
 	),
 
